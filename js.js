8e9d562c6a898cf5c7d17d9f86c2d25e8b170d94
Gus Caplan <me@gus.host>
2017-10-08 18:32:44 -0500

    first commit

---
+++ js.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+if (!process.argv[2]) return;
+
+let input = '';
+for (const item of process.argv.slice(2)) {
+  if (item[0] !== '-' || item[1] !== '-') input += item;
+  // its a switch
+}
+
+if (input[0] === '{') input = `(${input})`;
+
+if (process.stdin.isTTY) {
+  run('');
+} else {
+  let stdin = '';
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', (chunk) => {
+    stdin += chunk;
+  });
+  process.stdin.on('end', () => {
+    try {
+      run(JSON.parse(stdin));
+    } catch (err) {
+      run(stdin.trimRight());
+    }
+  });
+  process.stdin.resume();
+}
+
+function stdout(data) {
+  process.stdout.write(typeof data === 'string' ? data :
+    process.stdout.isTTY ?
+      require('util').inspect(data, { colors: true }) :
+      JSON.stringify(data));
+}
+
+function run(stdin) { // eslint-disable-line no-unused-vars
+  const ret = eval(input);
+  if (!input.includes('stdout')) stdout(ret);
+}
