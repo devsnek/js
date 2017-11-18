2f50ad50be5cd1819f03fe36f5619217bdf5502e
Gus Caplan <me@gus.host>
2017-11-18 11:07:46 -0600

    new eslint

--- js.js
+++ js.js
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
-if (!process.argv[2]) return;
+if (!process.argv[2])
+  return;
 
 const ttyin = Boolean(process.stdin.isTTY);
 const ttyout = Boolean(process.stdout.isTTY);
 
 let input = '';
 for (const item of process.argv.slice(2)) {
-  if (item[0] !== '-' || item[1] !== '-') input += item;
+  if (item[0] !== '-' || item[1] !== '-')
+    input += item;
   // its a switch
 }
 if (input[0] === '-' && input[1] === 'v' && input.length === 2) {
   stdout(`${require('./package.json').version}\n`);
   return;
 }
-if (input[0] === '{') input = `(${input})`;
+if (input[0] === '{')
+  input = `(${input})`;
 
 for (const name of require('repl')._builtinLibs) {
   const setReal = (val) => {
@@ -40,7 +43,8 @@ for (const name of require('repl')._builtinLibs) {
 
 if (ttyin) {
   run('');
-  if (ttyout) stdout('\n');
+  if (ttyout)
+    stdout('\n');
 } else {
   let stdin = '';
   process.stdin.setEncoding('utf8');
@@ -53,7 +57,8 @@ if (ttyin) {
     } catch (err) {
       run(stdin.trimRight());
     }
-    if (ttyout) stdout('\n');
+    if (ttyout)
+      stdout('\n');
   });
   process.stdin.resume();
 }
@@ -67,5 +72,6 @@ function stdout(data) {
 
 function run(stdin) { // eslint-disable-line no-unused-vars
   const ret = eval(input);
-  if (!input.includes('stdout')) stdout(ret);
+  if (!input.includes('stdout'))
+    stdout(ret);
 }
